725fdfd0ea2412aaa2fa42c2c24fd4bf2bac656b
tanishaaraoo07 <damalatanisha7@gmail.com>
2025-04-07 21:08:41 +0530

    added env config

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const dotenv = require('dotenv');
+
+dotenv.config({
+    path: '../.env',
+})
 
 app.get('/', (req, res) => {
   res.send('Hello from Node.js CI/CD pipeline!');
